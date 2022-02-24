17680f6c2e48c18559015f6915e1d19c6cd7dbb3
Tyler Sanbar <tylersanbar@gmail.com>
2022-02-23 20:02:03 -0600

    Starting random move agent

--- 2048-master/js/agent.js
+++ 2048-master/js/agent.js
@@ -1,3 +1,19 @@
 function Agent(){
-    
+    var state = "stopped";
+    this.listen();
 }
+
+Agent.prototype.startAgent = function () {
+    state = "started";
+    while(this.state == "started"){
+        //Make a move every second
+        setInterval(this.makeMove(), 1000);
+    }
+}
+
+Agent.prototype.makeMove = function () {
+    //Assigns move to a random integer from 0 to 3
+    var move = Math.floor(Math.random() * 4);
+        this.emit("move", move);
+}
+
