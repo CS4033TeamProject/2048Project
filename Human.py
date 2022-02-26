d510067e169ccafa69d8086d5e176ba6ac88f502
Tyler Sanbar <tylersanbar@gmail.com>
2022-02-26 16:46:06 -0600

    changed grid to 3x3

--- Human.py
+++ Human.py
@@ -18,6 +18,7 @@ class Human:
 
 if __name__ == "__main__":
     FILE_URL = "file:///C:/Users/kylew/Documents/Code/Machine%20Learning/2048%20RL/2048-master/index.html"
+    FILE_URL = "file:///C:/Users/tyler/OneDrive/Documents/OU/Spring 2022/Machine Learning/2048Project/2048-master/index.html"
 
     try:
         gigaChad = Human(FILE_URL, 4)
