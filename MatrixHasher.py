eaac5ef315ed895595b1cb8ff4b8f4091f9c3b4f
Kyle <Kyle.J.Weidner-1@ou.edu>
2022-02-26 17:35:49 -0600

    Actually works now

--- MatrixHasher.py
+++ MatrixHasher.py
@@ -5,30 +5,44 @@ class MatrixHasher:
     def matrixToString(self, matrix: list) -> str:
         return str(matrix)
     
-    def stringToMatrix(self, string: str) -> list:
+    def stringToMatrix(self, s: str) -> list:
         matrix = []
 
-        s = string.split("[")
+        # To chop off [ and ]
+        x = 1
+        while x < len(s)-1:
+            # Row var
+            if s[x] == "[":
+                temp = []
 
-        for i in range(0, len(s)):
-            if s[i] != "":
-                temp = s[i].split(",")
-                tempRow = []
+            if s[x].isdigit():
+                temp_s = s[x]
 
-                for j in range(0, len(temp)):
-                    # If its a number but not the last number
-                    if temp[j].isnumeric() and not "]" in temp[j]:
-                        tempRow.append(int(temp[j]))
-                    elif temp[j][0].isnumeric():
-                        tempRow.append(int(temp[j][0]))
-                        break
+                # For more than 1 digit
+                y = 1
+                while s[x+y].isdigit():
+                    temp_s += s[x+y]
+                    y += 1
                 
-                matrix.append(tempRow)
+                # If number is more than one digit skip next chars
+                if y > 1:
+                    x = x + y
+                
+                temp.append(int(temp_s))
+
+            # If end of row
+            if s[x] == "]":
+                matrix.append(temp)
+
+            x += 1
         
         return matrix
-                
+
+
 
 if __name__ == "__main__":
     test = MatrixHasher()
-    print(test.stringToMatrix("[[1,2,3],[45,6,7]]"))
-    print(test.stringToMatrix("[[13,4,5],[89,8],[7,5,3]]"))
+    sm = test.matrixToString([[13,4,5],[89,8,6],[7,5,3]])
+    print(sm)
+    sm = test.stringToMatrix(sm)
+    print(sm)
