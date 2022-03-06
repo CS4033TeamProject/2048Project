050524625646a242f1e91f888336bb560edf748a
Kyle <Kyle.J.Weidner-1@ou.edu>
2022-03-06 17:28:26 -0600

    Added exception for closing selenium

--- Agent.py
+++ Agent.py
@@ -11,16 +11,22 @@ from MonteCarlo import MonteCarlo
 from GLIEMonteCarlo import GLIEMonteCarlo
 from datetime import datetime
 
+import selenium
+
 from BrowserInterface import Interface
 from MatrixHasher import MatrixHasher
 
 if __name__ == "__main__":
-    GAME_URL = "file:" + os.getcwd() + "/2048-master/index.html"
-    DATABASE_URL = "file:" + os.getcwd() + "/2048-master/database.pickle"
-    database = Database()
-    interface = Interface(GAME_URL, 3, 64)
-    environment = Environment(interface = interface ,database=database)
-    policy = Policy()
-    iterations = 1000
-    algorithm = GLIEMonteCarlo(environment, database, policy, iterations)
-    database.save_db()
+    try:
+        GAME_URL = "file:" + os.getcwd() + "/2048-master/index.html"
+        DATABASE_URL = "file:" + os.getcwd() + "/2048-master/database.pickle"
+        database = Database()
+        interface = Interface(GAME_URL, 3, 64)
+        environment = Environment(interface = interface ,database=database)
+        policy = Policy()
+        iterations = 1000
+        algorithm = GLIEMonteCarlo(environment, database, policy, iterations)
+        database.save_db()
+    except selenium.common.exceptions.NoSuchWindowException:
+        database.save_db()
+        print("Closed!")
